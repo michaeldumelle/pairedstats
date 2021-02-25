e085c567a40f14445987127cbf4c561061222704
Michael Dumelle <mjdumelle@gmail.com>
2021-02-25 14:14:27 -0800

    minor beta updates

--- R/pairedstats.R
+++ R/pairedstats.R
@@ -1,5 +1,6 @@
-pairedstats <- function(.data, .var, .val, .group, .fun = "mean", .p1 = NULL, .p2 = NULL, .pexpand = TRUE, .fun_aggregate = mean, slim = TRUE, ...){
+pairedstats <- function(.data, .var, .val, .group, .fun = "difference", .pair1 = NULL, .pair2 = NULL, .pair_expand = TRUE, .fun_aggregate = "mean", .slim = TRUE, ...){
 
+  # change factors to characters
   .data[] <- lapply(.data, function(x){
     if (is.factor(x)) {
       x <- as.character(x)
@@ -8,90 +9,151 @@ pairedstats <- function(.data, .var, .val, .group, .fun = "mean", .p1 = NULL, .p
     }
   })
 
-  if (is.null(.p1)) {
-    .p1 <- unique(.data[[.var]])
+  # take .pair1 to be all unique values if no values supplied
+  if (is.null(.pair1)) {
+    .pair1 <- unique(.data[[.var]])
   }
 
-  if (is.null(.p2)) {
-    .p2 <- .p1
+  # take .pair2 to be .pair1 if no values supplied
+  if (is.null(.pair2)) {
+    .pair2 <- .pair1
   }
 
+  # create index data with available pair combinations
+  index_data <- get_index_data(.pair1, .pair2, .pair_expand)
+
+  # split index data by copy and no copy
+  index_split <- split(index_data, index_data[["copy"]])
+
+  # take no copies
+  index_nocopy <- index_split$`FALSE`
+
+  # find unique values to use in subsetting
+  unique_pairvals <- unique(c(.pair1, .pair2))
+
+  # find unique groups
+  unique_groups <- unique(.data[[.group]])
+
+  # subset data to only include relevant values and columns
+  .data <- .data[.data[[.var]] %in% unique_pairvals, c(.var, .val, .group), drop = FALSE]
+
+  # aggregate data for a summary statistic by fun_aggregate
+  aggregated_data <- aggregate(
+    x = list(.val = .data[[.val]]),
+    by = list(.var = .data[[.var]], .group = .data[[.group]]),
+    FUN = .fun_aggregate,
+    drop = TRUE,
+    ...
+  )
 
 
-  .data <- .data[.data[[.var]] %in% unique(c(.p1, .p2)), c(.var, .val, .group), drop = FALSE]
+  paired_data <- get_paired_data(index_nocopy, aggregated_data, unique_groups)
 
-  aggregated_data <- aggregate(x = list(.val = .data[[.val]]), by = list(.var = .data[[.var]], .group = .data[[.group]]), FUN = .fun_aggregate, drop = TRUE, ...)
+  paired_splits <- split(paired_data, paired_data$index)
 
-  index_data <- get_index_data(.p1, .p2, .pexpand)
+  stats_list <- lapply(paired_splits, .fun, ...)
 
-  split_index <- split(index_data, index_data$copy)
+  stats_df <- do.call("rbind", stats_list)
 
-  nocopies <- split_index$`FALSE`
+  pairedstats_data <- get_pairedstats_data(stats_df, .slim, .fun, index_data, index_nocopy)
 
-  expand_data <- expand.grid(index = nocopies$index, .group = unique(.data[[.group]]))
 
-  expand_data$.p1 <- substring(expand_data$index, 1, 1)
-  expand_data$.p2 <- substring(expand_data$index, 2, 2)
 
-  expand_data <- merge(expand_data, aggregated_data, by.x = c(".p1", ".group"), by.y = c(.var, .group))
-  expand_data <- merge(expand_data, aggregated_data, by.x = c(".p2", ".group"), by.y = c(.var, .group), suffix = c("1", "2"))
-
-  expand_splits <- split(expand_data, expand_data$index)
-
-  nocopies$value <- unlist(lapply(expand_splits, function(x) mean(x[[paste0(.val, "1")]] - x[[paste0(.val, "2")]]), ...))
-  nocopies$pairs <- unlist(lapply(expand_splits, function(x) nrow(x)))
-  if (slim) {
-    final <- nocopies
-  } else {
-    final <- merge(index_data, nocopies[, c("index", "value", "pairs"), drop = FALSE])
-    final$value <- ifelse(final$copy, -1 * final$value, final$value)
+  if (.fun %in% c("difference")) {
+    pairedstats_class <- "pairedscalar"
+    pairedstats_data <- pairedstats_data[, c(".pair1", ".pair2", ".group", ".val1", ".val2", "metric"), drop = FALSE]
+    colnames(pairedstats_data) <- c(".pair1", ".pair2", .group, paste0("agg_", .val, "1"), paste0("agg_", .val, "2"), "metric")
+  } else if (.fun == "correlation") {
+    pairedstats_class <- "corrclass"
+    pairedstats_data <- pairedstats_data[, c(".pair1", ".pair2", "metric"), drop = FALSE]
+    if (any(is.na(pairedstats_data$metric))) message("Warning: At least one set of pairs had fewer than 3 observations -- returning NA's for that pair")
+    #colnames(pairedstats_data) <- c(".pair1", ".pair2", "metric")
   }
-  final[, c(".p1", ".p2", "value", "pairs"), drop = FALSE]
-  # get pairs
-  #paired_data <- get_paired_data(.data = .data, .var = .var, .val = .val, .group = .group, .p1 = .p1, .p2 = .p2, .pexpand = .pexpand, .fun_aggregate = .fun_aggregate, ...)
 
-  # compute pair statistics
+  pairedstats_data <- pairedstats_data[with(pairedstats_data, order(.pair1, .pair2)), , drop = FALSE]
+  pairedstats_data <- structure(pairedstats_data, class = c(pairedstats_class, class(pairedstats_data)))
 }
 
-get_index_data <- function(.p1, .p2, .pexpand) {
 
-  if (.pexpand) {
-    index_data <- expand.grid(.p2 = .p2, .p1 = .p1, stringsAsFactors = FALSE)
+
+### helpers
+
+difference <- function(paired_split) {
+  paired_split$metric <- paired_split[[".val1"]] - paired_split[[".val2"]]
+  paired_split[, c("index", ".group", ".val1", ".val2", "metric"), drop = FALSE]
+}
+
+correlation <- function(paired_split, ...) {
+  paired_split_small <- unique(paired_split[, c("index", ".pair1", ".pair2"), drop = FALSE])
+  if (nrow(paired_split) >= 3) {
+    paired_split_small$metric <- list(cor.test(paired_split$.val1, paired_split$.val2, ...))
   } else {
-    index_data <- data.frame(.p1 = .p1, .p2 = .p2)
+    paired_split_small$metric <- NA
+  }
+  paired_split_small[, c("index", ".pair1", ".pair2", "metric"), drop = FALSE]
+}
+
+get_pairedstats_data <- function(stats_df, .slim, .fun, index_data, index_data_nocopy) {
+  nonpaircols <- which(!(colnames(stats_df) %in% c(".pair1", ".pair2")))
+  if (.slim) {
+    index_data_nocopy_small <- index_data_nocopy[, c("index", "copy", ".pair1", ".pair2"), drop = FALSE]
+    pairedstats_data <- merge(index_data_nocopy_small,stats_df[, nonpaircols, drop = FALSE], by = "index")
+  } else {
+    index_data_small <- index_data[, c("index", "copy" , ".pair1", ".pair2"), drop = FALSE]
+    pairedstats_data <- merge(index_data_small, stats_df[, nonpaircols, drop = FALSE], by = "index")
+    if (.fun %in% c("difference")) {
+      pairedstats_data$metric <- ifelse(pairedstats_data$copy, -1 * pairedstats_data$metric, pairedstats_data$metric)
+    }
+  }
+  pairedstats_data
+}
+
+get_index_data <- function(.pair1, .pair2, .pair_expand) {
+
+  if (.pair_expand) {
+    index_data <- expand.grid(.pair2 = .pair2, .pair1 = .pair1, stringsAsFactors = FALSE)
+    index_data <- index_data[, c(".pair1", ".pair2"), drop = FALSE]
+  } else {
+    index_data <- data.frame(.pair1 = .pair1, .pair2 = .pair2, stringsAsFactors = FALSE)
   }
 
-  index_data <-  subset(index_data, .p1 != .p2)
-  index_data$index <- paste0(pmin(index_data$.p1, index_data$.p2), pmax(index_data$.p1, index_data$.p2))
+  # remove pairs with self
+  index_data <-  index_data[index_data$.pair1 != index_data$.pair2, , drop = FALSE]
+
+  # create pair index
+  index_data$index <- paste0(
+    pmin(index_data$.pair1, index_data$.pair2),
+    pmax(index_data$.pair1, index_data$.pair2)
+  )
+
+  # create a duplicated indicator (FALSE = not a copy)
   index_data$copy <- duplicated(index_data$index)
+
+  # return
   index_data
 }
 
-get_expand_data <- function(nocopy, group) {
-  expand_data <- expand_grid(nocopy)
+get_paired_data <- function(index_nocopy, aggregated_data, unique_groups) {
+  # get expanded index data
+  expand_index <- expand.grid(index = index_nocopy$index, .group = unique_groups, stringsAsFactors = FALSE)
+
+  # create pair data
+  expand_index$.pair1 <- substring(expand_index$index, 1, 1)
+  expand_index$.pair2 <- substring(expand_index$index, 2, 2)
+
+  paired_data <- merge(
+    expand_index,
+    aggregated_data,
+    by.x = c(".pair1", ".group"),
+    by.y = c(".var", ".group")
+  )
+  paired_data <- merge(
+    paired_data,
+    aggregated_data,
+    by.x = c(".pair2", ".group"),
+    by.y = c(".var", ".group"),
+    suffix = c("1", "2")
+  )
+
+  paired_data
 }
-
-# get_paired_data <- function(.data, .var, .val, .group, .p1, .p2, .pexpand, .fun_aggregate, ...){
-#
-#
-#
-# }
-
-# get_aggregated_data <- function(.data, .var, .val, .group, .fun_aggregate, ...) {
-#   if (is.null(.group)) {
-#     return(aggregate(x = list(.val = .data[[.val]]), by = list(.var = .data[[.var]]), FUN = .fun_aggregate, drop = FALSE, ...))
-#   } else {
-#     return(aggregate(x = list(.val = .data[[.val]]), by = list(.var = .data[[.var]], .group = .data[[.group]]), FUN = .fun_aggregate, drop = FALSE, ...))
-#   }
-# }
-
-
-# 1. subset relevant values
-# 2. get aggregated data
-# 3. get expanded data
-# 4. get merged data
-# 4.a. only consider appropriate subset
-# 5. split on .p1 and .p2
-# 6. do the correct function
-# 6.a. only to the appropriate subset
-# 7 types could be difference, difference_log, ratio, ratio_log, correlation
---
+++ R/summary.R
@@ -0,0 +1,41 @@
+summary.pairedscalar <- function(object, ...){
+  paired_data_split <- split(object, list(.pair1 = object$.pair1, .pair2 = object$.pair2), drop = TRUE)
+  paired_data_summary <- lapply(paired_data_split, function(x) {
+    unq_df <- unique(x[, c(".pair1", ".pair2"), drop = FALSE])
+    unq_df$Min <- min(x$metric)
+    unq_df$Q1 <- quantile(x$metric, 0.25)
+    unq_df$Median <- quantile(x$metric, 0.5)
+    unq_df$Mean <- mean(x$metric)
+    unq_df$Q3 <- quantile(x$metric, 0.75)
+    unq_df$Max <- max(x$metric)
+    unq_df$n_pairs <- length(x$metric)
+    unq_df
+  })
+  paired_stats_summary <- do.call("rbind", paired_data_summary)
+  paired_stats_summary <- paired_stats_summary[with(paired_stats_summary, order(.pair1, .pair2)), , drop = FALSE]
+  row.names(paired_stats_summary) <- NULL
+  paired_stats_summary
+}
+
+
+summary.corrclass <- function(object, p.adjust = "none", ...){
+  paired_data_split <- split(object, list(.pair1 = object$.pair1, .pair2 = object$.pair2), drop = TRUE)
+  paired_data_summary <- lapply(paired_data_split, function(x) {
+    unq_df <- unique(x[, c(".pair1", ".pair2"), drop = FALSE])
+    if (is.na(x$metric)) {
+      unq_df$Cor_Est <- NA
+      unq_df$p_value <- NA
+    } else {
+      unq_df$Cor_Est <- x$metric[[1]]$estimate
+      unq_df$p_value <- x$metric[[1]]$p.value
+    }
+    unq_df
+  })
+  paired_stats_summary <- do.call("rbind", paired_data_summary)
+  paired_stats_summary <- paired_stats_summary[with(paired_stats_summary, order(.pair1, .pair2)), , drop = FALSE]
+  if (p.adjust != "none") {
+    paired_stats_summary$p_value_adj <- p.adjust(paired_stats_summary$p_value, method = p.adjust, n = sum(!is.na(paired_stats_summary$p_value)))
+  }
+  row.names(paired_stats_summary) <- NULL
+  paired_stats_summary
+}
